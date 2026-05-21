cbbe515d3288ca5129630d7e99b3649963707df6
vipins1234 <vipinkumar05081999@gmail.com>
2026-05-21 13:33:37 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -247,7 +247,7 @@ async def save(client: Client, message: Message):
         return
     
     if "https://t.me/" in message.text:
-        if batch_temp.IS_BATCH.get(message.from_user.id) == False:
+        if batch_temp.IS_BATCH.get(message.from_user.id, True) == False:
             return await message.reply_text("**One Task Is Already Processing. Wait For Complete It. If You Want To Cancel This Task Then Use - /cancel**")
         datas = message.text.split("/")
         temp = datas[-1].replace("?single","").split("-")
