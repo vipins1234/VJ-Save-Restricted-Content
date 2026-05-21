b3294a85694bf6efe441708566354f1b8a8fe101
vipins1234 <vipinkumar05081999@gmail.com>
2026-05-21 07:39:56 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -9,6 +9,11 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
 from config import API_ID, API_HASH, ERROR_MESSAGE, LOGIN_SYSTEM, STRING_SESSION, CHANNEL_ID, WAITING_TIME
+class BatchState:
+    WAITING_START = {}   # user_id : True/False
+    WAITING_END = {}     # user_id : True/False
+    START_ID = {}        # user_id : (link, fromID)
+    END_ID = {}          # user_id : toID
 from database.db import db
 from TechVJ.strings import HELP_TXT
 from bot import TechVJUser
