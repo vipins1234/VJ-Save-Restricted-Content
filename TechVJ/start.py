f57513580082f0e2404a98061cafbdc876b4b1bf
vipins1234 <vipinkumar05081999@gmail.com>
2026-05-22 18:41:52 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -475,7 +475,9 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
     caption = f"📦 Remaining : {remaining}\n\n{msg.caption}"
 else:
     caption = f"📦 Remaining : {remaining}"
-    if batch_temp.IS_BATCH.get(message.from_user.id): return 
+
+if batch_temp.IS_BATCH.get(message.from_user.id):
+    return 
             
     if "Document" == msg_type:
         try:
